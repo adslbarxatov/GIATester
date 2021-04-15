be9754adc0937853819f115e27228e249ec8c9da
Nicolay B. aka RD_AAOW <adslbarxatov@mail.ru>
2021-04-15 23:13:16 +1100

    Xash3D support

--- source code/pm_shared/pm_materials.h
+++ source code/pm_shared/pm_materials.h
@@ -17,16 +17,18 @@
 
 #define CBTEXTURENAMEMAX		13 		// only load first n chars of name
 
-#define CHAR_TEX_CONCRETE		'C'		// texture types
+#define CHAR_TEX_CONCRETE	'C'		// texture types
 #define CHAR_TEX_METAL		'M'
 #define CHAR_TEX_DIRT		'D'
 #define CHAR_TEX_VENT		'V'
 #define CHAR_TEX_GRATE		'G'
-#define CHAR_TEX_TILE		'T'
+#define CHAR_TEX_RUBBER		'R'
+#define CHAR_TEX_TILES		'T'
 #define CHAR_TEX_SLOSH		'S'
 #define CHAR_TEX_WOOD		'W'
-#define CHAR_TEX_COMPUTER		'P'
+#define CHAR_TEX_COMPUTER	'P'
 #define CHAR_TEX_GLASS		'Y'
 #define CHAR_TEX_FLESH		'F'
+#define CHAR_TEX_SNOW		'N'
 
 #endif//PM_MATERIALS_H
