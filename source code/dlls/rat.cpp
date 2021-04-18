c4eee225580c6b32f7424274d479b9350cc48af8
Nicolay B. aka RD_AAOW <adslbarxatov@mail.ru>
2021-04-18 17:07:09 +1100

    Xash

--- source code/dlls/rat.cpp
+++ source code/dlls/rat.cpp
@@ -1,9 +1,9 @@
 /***
 *
 *	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
-*	
-*	This product contains software technology licensed from Id 
-*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*
+*	This product contains software technology licensed from Id
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
 *	All Rights Reserved.
 *
 *   This source code contains proprietary and confidential information of
@@ -26,72 +26,72 @@
 // Monster's Anim Events Go Here
 //=========================================================
 
-class CRat : public CBaseMonster
-{
-public:
-	void Spawn( void );
-	void Precache( void );
-	void SetYawSpeed( void );
-	int  Classify ( void );
-};
-LINK_ENTITY_TO_CLASS( monster_rat, CRat );
+class CRat: public CBaseMonster
+	{
+	public:
+		void Spawn (void);
+		void Precache (void);
+		void SetYawSpeed (void);
+		int  Classify (void);
+	};
+LINK_ENTITY_TO_CLASS (monster_rat, CRat);
 
 //=========================================================
 // Classify - indicates this monster's place in the 
 // relationship table.
 //=========================================================
-int	CRat :: Classify ( void )
-{
+int	CRat::Classify (void)
+	{
 	return	CLASS_INSECT;
-}
+	}
 
 //=========================================================
 // SetYawSpeed - allows each sequence to have a different
 // turn rate associated with it.
 //=========================================================
-void CRat :: SetYawSpeed ( void )
-{
+void CRat::SetYawSpeed (void)
+	{
 	int ys;
 
-	switch ( m_Activity )
-	{
-	case ACT_IDLE:
-	default:
-		ys = 45;
-		break;
-	}
+	switch (m_Activity)
+		{
+		case ACT_IDLE:
+		default:
+			ys = 45;
+			break;
+		}
 
 	pev->yaw_speed = ys;
-}
+	}
 
 //=========================================================
 // Spawn
 //=========================================================
-void CRat :: Spawn()
-{
-	Precache( );
+void CRat::Spawn ()
+	{
+	Precache ();
 
-	SET_MODEL(ENT(pev), "models/bigrat.mdl");
-	UTIL_SetSize( pev, Vector( 0, 0, 0 ), Vector( 0, 0, 0 ) );
+	SET_MODEL (ENT (pev), "models/bigrat.mdl");
+	UTIL_SetSize (pev, Vector (0, 0, 0), Vector (0, 0, 0));
 
-	pev->solid			= SOLID_SLIDEBOX;
-	pev->movetype		= MOVETYPE_STEP;
-	m_bloodColor		= BLOOD_COLOR_RED;
-	pev->health			= 8;
-	pev->view_ofs		= Vector ( 0, 0, 6 );// position of the eyes relative to monster's origin.
-	m_flFieldOfView		= 0.5;// indicates the width of this monster's forward view cone ( as a dotproduct result )
-	m_MonsterState		= MONSTERSTATE_NONE;
+	pev->solid = SOLID_SLIDEBOX;
+	pev->movetype = MOVETYPE_STEP;
+	m_bloodColor = BLOOD_COLOR_RED;
+	pev->health = 8;
+	pev->view_ofs = Vector (0, 0, 6);// position of the eyes relative to monster's origin.
+	m_flFieldOfView = 0.5;// indicates the width of this monster's forward view cone ( as a dotproduct result )
+	m_MonsterState = MONSTERSTATE_NONE;
 
-	MonsterInit();
-}
+	MonsterInit ();
+	}
 
 //=========================================================
 // Precache - precaches all resources this monster needs
 //=========================================================
-void CRat :: Precache()
-{
-	PRECACHE_MODEL("models/bigrat.mdl");
-}	
+void CRat::Precache ()
+	{
+	PRECACHE_MODEL ("models/bigrat.mdl");
+	}
 
 //=========================================================
 // AI Schedules Specific to this monster
