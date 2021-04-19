c0ffd67a1be3783857e5d198fd7cb6a4adb7b8e5
Nicolay B. aka RD_AAOW <adslbarxatov@mail.ru>
2021-04-20 00:05:15 +1100

    Xash

--- source code/cl_dll/overview.h
+++ source code/cl_dll/overview.h
@@ -13,19 +13,19 @@
 //-----------------------------------------------------------------------------
 // Purpose: Handles the drawing of the top-down map and all the things on it
 //-----------------------------------------------------------------------------
-class CHudOverview : public CHudBase
-{
-public:
-	int Init();
-	int VidInit();
+class CHudOverview: public CHudBase
+	{
+	public:
+		int Init ();
+		int VidInit ();
 
-	int Draw(float flTime);
-	void InitHUDData( void );
+		int Draw (float flTime);
+		void InitHUDData (void);
 
-private:
-	HSPRITE m_hsprPlayer;
-	HSPRITE m_hsprViewcone;
-};
+	private:
+		HSPRITE m_hsprPlayer;
+		HSPRITE m_hsprViewcone;
+	};
 
 
 #endif // OVERVIEW_H
