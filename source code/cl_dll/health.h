f1ab8386fb5b91adfb9b0be4af5421daecbe9aec
Nicolay B. aka RD_AAOW <adslbarxatov@mail.ru>
2021-04-19 23:22:25 +1100

    Xash

--- source code/cl_dll/health.h
+++ source code/cl_dll/health.h
@@ -1,9 +1,9 @@
 /***
 *
 *	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
-*	
-*	This product contains software technology licensed from Id 
-*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*
+*	This product contains software technology licensed from Id
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
 *	All Rights Reserved.
 *
 *   Use, distribution, and modification of this source code and/or resulting
@@ -89,39 +89,39 @@
 
 
 typedef struct
-{
+	{
 	float fExpire;
 	float fBaseline;
 	int	x, y;
-} DAMAGE_IMAGE;
-	
+	} DAMAGE_IMAGE;
+
 //
 //-----------------------------------------------------
 //
 class CHudHealth: public CHudBase
-{
-public:
-	virtual int Init( void );
-	virtual int VidInit( void );
-	virtual int Draw(float fTime);
-	virtual void Reset( void );
-	int MsgFunc_Health(const char *pszName,  int iSize, void *pbuf);
-	int MsgFunc_Damage(const char *pszName,  int iSize, void *pbuf);
-	int m_iHealth;
-	int m_HUD_dmg_bio;
-	int m_HUD_cross;
-	float m_fAttackFront, m_fAttackRear, m_fAttackLeft, m_fAttackRight;
-	void GetPainColor( int &r, int &g, int &b );
-	float m_fFade;
+	{
+	public:
+		virtual int Init (void);
+		virtual int VidInit (void);
+		virtual int Draw (float fTime);
+		virtual void Reset (void);
+		int MsgFunc_Health (const char* pszName, int iSize, void* pbuf);
+		int MsgFunc_Damage (const char* pszName, int iSize, void* pbuf);
+		int m_iHealth;
+		int m_HUD_dmg_bio;
+		int m_HUD_cross;
+		float m_fAttackFront, m_fAttackRear, m_fAttackLeft, m_fAttackRight;
+		void GetPainColor (int& r, int& g, int& b);
+		float m_fFade;
 
-private:
-	HSPRITE m_hSprite;
-	HSPRITE m_hDamage;
-	
-	DAMAGE_IMAGE m_dmg[NUM_DMG_TYPES];
-	int	m_bitsDamage;
-	int DrawPain(float fTime);
-	int DrawDamage(float fTime);
-	void CalcDamageDirection(vec3_t vecFrom);
-	void UpdateTiles(float fTime, long bits);
-};	
+	private:
+		HSPRITE m_hSprite;
+		HSPRITE m_hDamage;
+
+		DAMAGE_IMAGE m_dmg[NUM_DMG_TYPES];
+		int	m_bitsDamage;
+		int DrawPain (float fTime);
+		int DrawDamage (float fTime);
+		void CalcDamageDirection (vec3_t vecFrom);
+		void UpdateTiles (float fTime, long bits);
+	};
